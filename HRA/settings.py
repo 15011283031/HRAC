4967eb78ad43657d2a4aa852ad67a0ee2b9c4b14
peter.q.chen <peterchenhra@hotmail.com>
2018-04-23 00:08:51 +0800

    add 182.254.151.112

--- HRA/settings.py
+++ HRA/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'f+z$ot%kbzddyzc-u6ep*m096)8zhmkvmx%tnrqlqjwgul(_-s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "182.254.151.112"]
 
 
 # Application definition
